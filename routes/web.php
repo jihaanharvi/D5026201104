7cae4c1368459ef64a546a880136bdf0cf1d77cf
Jihaan Harvi Habibah <91890681+jihaanharvi@users.noreply.github.com>
2021-11-21 19:46:28 +0700

    Membuat CRUD dengan Laravel
    Melakukan CRUD dengan Laravel sesuai dengan penjelasan yang ada pada web malasngoding

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,12 @@ Route::get('ets2021',"ViewController@showETS") ;
 
 Route::get('tugas_php',"ViewController@showPHP") ;
 Route::post('hasil',"ViewController@Hasil") ;
+
+//roue CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
